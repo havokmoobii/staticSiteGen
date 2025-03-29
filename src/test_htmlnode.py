e0c0c22f376325eaace583f9971e9d30fffebb64
HavokMoobii <jasonmcounter@gmail.com>
2025-03-29 12:31:59 -0500

    Added LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -8,9 +8,14 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("p")
         node2 = HTMLNode("p", "This is some text")
         node3 = HTMLNode("a", "This is a link", None, {"href": "https://www.google.com", "target": "_blank"})
-        print(node)
-        print(node2)
-        print(node3)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        node1 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node1.to_html(), '<a href="https://www.google.com">Click me!</a>')
+        node2 = LeafNode("h1", "This is a header")
+        print(node2.to_html())
     
 if __name__ == "__main__":
     unittest.main()
