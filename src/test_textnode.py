85a0cf421b903515f35450300fbf8c192bbe6f02
HavokMoobii <jasonmcounter@gmail.com>
2025-03-31 05:03:46 -0500

    Added split_nodes_delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -15,6 +15,44 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node4, node5)
         self.assertNotEqual(node, node4)
     
+    def test_split_nodes(self):
+        nodes = [TextNode("This is text with a **bolded phrase** in the middle", TextType.TEXT)]
+        test_nodes = [
+            TextNode("This is text with a", TextType.TEXT),
+            TextNode("bolded phrase", TextType.BOLD),
+            TextNode("in the middle", TextType.TEXT)
+        ]
+        new_nodes = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        self.assertEqual(test_nodes, new_nodes)
+        nodes2 = [TextNode("This is text with a _italic phrase_ in the middle", TextType.TEXT)]
+        test_nodes2 = [
+            TextNode("This is text with a", TextType.TEXT),
+            TextNode("italic phrase", TextType.ITALIC),
+            TextNode("in the middle", TextType.TEXT)
+        ]
+        new_nodes2 = split_nodes_delimiter(nodes2, "_", TextType.ITALIC)
+        self.assertEqual(test_nodes2, new_nodes2)
+        nodes3 = [TextNode("This is text with a ```code``` in the middle", TextType.TEXT)]
+        test_nodes3 = [
+            TextNode("This is text with a", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode("in the middle", TextType.TEXT)
+        ]
+        new_nodes3 = split_nodes_delimiter(nodes3, "`", TextType.CODE)
+        self.assertEqual(test_nodes3, new_nodes3)
+        nodes4 = [TextNode("This is text with a **bolded phrase** an _italic phrase_ and some `code`", TextType.TEXT)]
+        test_nodes4 = [
+            TextNode("This is text with a", TextType.TEXT),
+            TextNode("bolded phrase", TextType.BOLD),
+            TextNode("an", TextType.TEXT),
+            TextNode("italic phrase", TextType.ITALIC),
+            TextNode("and some", TextType.TEXT),
+            TextNode("code", TextType.CODE)
+        ]
+        new_nodes4 = split_nodes_delimiter(nodes4, "*", TextType.BOLD)
+        new_nodes4 = split_nodes_delimiter(new_nodes4, "_", TextType.ITALIC)
+        new_nodes4 = split_nodes_delimiter(new_nodes4, "`", TextType.CODE)
+        self.assertEqual(test_nodes4, new_nodes4)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -23,3 +23,43 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+
+    # Probably still need to figure out how to handles newlines in code markdown.
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    nodes = []
+
+    for node in old_nodes:
+
+        if node.text_type != TextType.TEXT:
+            nodes.append(node)
+        else:    
+            node_start_index = 0
+            node_current_index = -1
+            delimiter_found = False
+            words = node.text.split()
+
+            for word in words:
+                node_current_index += 1
+
+                if word.startswith(delimiter):
+                    # Prevent trying to create an empty node if delimiter is at start of node.
+                    if node_current_index != 0:
+                        nodes.append(TextNode(" ".join(words[node_start_index:node_current_index]), TextType.TEXT))
+                    node_start_index = node_current_index
+                    delimiter_found = True
+
+                if word.endswith(delimiter) and delimiter_found == True:
+                    line = " ".join(words[node_start_index:node_current_index + 1])
+                    nodes.append(TextNode(line.strip(delimiter), text_type))
+                    # Set start index to next word.
+                    node_start_index = node_current_index + 1
+                    delimiter_found = False
+
+                if node_current_index == len(words) - 1:
+                    if delimiter_found == True:
+                        raise Exception(f"Invalid Markdown syntax. Missing closing {delimiter}.")
+                    if node_start_index <= node_current_index:
+                        nodes.append(TextNode(" ".join(words[node_start_index:]), TextType.TEXT))
+
+    return nodes
