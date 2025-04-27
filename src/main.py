51a93fd9e7addf6bffc99b91c078fa8a0c5a0245
HavokMoobii <jasonmcounter@gmail.com>
2025-04-26 22:45:25 -0500

    Restarting project due to procrastination

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from textnode import TextNode, TextType
-
-test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-test2 = TextNode("This is some anchor text", TextType.CODE, "https://www.boot.dev")
-test3 = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-
-print(test)
-print(test == test2)
-print(test == test3)
