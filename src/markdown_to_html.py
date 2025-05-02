f9f71f5193a9a1ee35d79973ea4648dde77c4bcb
HavokMoobii <jasonmcounter@gmail.com>
2025-05-02 00:00:39 -0500

    Added markdown_to_html_node and some helper functions

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from markdown_to_textnode import text_to_textnodes
+from textnode import text_node_to_html_node
+from htmlnode import ParentNode, LeafNode
 
 class BlockType(Enum):
     PARAGRAPH = "Paragraph"
@@ -53,3 +55,78 @@ def block_to_block_type(block):
     if ordered:
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+
+    block_nodes = []
+
+    for block in blocks:
+        type = block_to_block_type(block)
+        tag = block_to_tag(block, type)
+        
+        if type == BlockType.CODE:
+            children = [LeafNode(None, f"<code>{block.strip("```").lstrip("\n")}</code>")]
+        else:
+            formatted_block = format_block_text(block, type)
+            children = []
+            child_nodes = text_to_textnodes(formatted_block)
+            for node in child_nodes:
+                children.append(text_node_to_html_node(node))
+        block_nodes.append(ParentNode(tag, children))
+
+    return ParentNode("div", block_nodes)
+
+def format_block_text(text, type):
+    output = text
+    if type == BlockType.PARAGRAPH or type == BlockType.HEADING:
+        split_block = output.split("\n")
+        output = " ".join(split_block)
+    output = output.strip("#")
+    if type == BlockType.QUOTE or type == BlockType.UNORDERED_LIST or type == BlockType.ORDERED_LIST:
+        split_block = output.split("\n")
+        new_block = ""
+        for line in split_block:
+            match(type):
+                case(BlockType.QUOTE):
+                    new_block = new_block + line[1:] + "\n"
+                case(BlockType.UNORDERED_LIST):
+                    new_block = new_block + f"<li>{line[2:]}</li>\n"
+                case(BlockType.ORDERED_LIST):
+                    new_block = new_block + f"<li>{line[3:]}</li>\n"
+        output = new_block
+    return output.strip()
+    
+
+def trim_block_newlines(block):
+    split_block = block.split("\n")
+    return " ".join(split_block)
+
+def block_to_tag(block, block_type):
+    match(block_type):
+        case(BlockType.PARAGRAPH):
+            return "p"
+        case(BlockType.HEADING):
+            if block.startswith("# "):
+                return "h1"
+            if block.startswith("## "):
+                return "h2"
+            if block.startswith("### "):
+                return "h3"
+            if block.startswith("#### "):
+                return "h4"
+            if block.startswith("##### "):
+                return "h5"
+            if block.startswith("###### "):
+                return "h6"
+            raise Exception("Heading block type on a non-heading block")
+        case(BlockType.CODE):
+            return "pre"
+        case(BlockType.QUOTE):
+            return "blockquote"
+        case(BlockType.UNORDERED_LIST):
+            return "ul"
+        case(BlockType.ORDERED_LIST):
+            return "ol"
+        case _:
+            raise Exception("Invalid BlockType")
--- src/test_markdown_to_html.py
+++ src/test_markdown_to_html.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_to_html import BlockType, markdown_to_blocks, block_to_block_type
+from markdown_to_html import BlockType, markdown_to_blocks, block_to_block_type, block_to_tag, markdown_to_html_node
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_eq(self):
@@ -70,7 +70,132 @@ class TestBlockToBlockType(unittest.TestCase):
         self.assertNotEqual(BlockType.ORDERED_LIST, block_to_block_type("2. Test\n3. more test\n4. even more test"))
         self.assertNotEqual(BlockType.ORDERED_LIST, block_to_block_type("1.Test\n2.more test\n3.even more test"))
 
+class TestBlockToTag(unittest.TestCase):
+    def test_tags(self):
+        self.assertEqual(block_to_tag("Literally whatever", BlockType.PARAGRAPH), "p")
+        self.assertEqual(block_to_tag("# Literally whatever", BlockType.HEADING), "h1")
+        self.assertEqual(block_to_tag("## Literally whatever", BlockType.HEADING), "h2")
+        self.assertEqual(block_to_tag("### Literally whatever", BlockType.HEADING), "h3")
+        self.assertEqual(block_to_tag("#### Literally whatever", BlockType.HEADING), "h4")
+        self.assertEqual(block_to_tag("##### Literally whatever", BlockType.HEADING), "h5")
+        self.assertEqual(block_to_tag("###### Literally whatever", BlockType.HEADING), "h6")
+        self.assertRaises(Exception, block_to_tag, "####### Literally whatever", BlockType.HEADING)
+        self.assertEqual(block_to_tag("Literally whatever", BlockType.CODE), "pre")
+        self.assertEqual(block_to_tag("Literally whatever", BlockType.QUOTE), "blockquote")
+        self.assertEqual(block_to_tag("Literally whatever", BlockType.UNORDERED_LIST), "ul")
+        self.assertEqual(block_to_tag("Literally whatever", BlockType.ORDERED_LIST), "ol")
+        self.assertRaises(Exception, block_to_tag, "####### Literally whatever", "Not a BlockType")
 
+class TestMarkdownToHtmlNode(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+           html,
+           "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_heading(self):
+        md = """
+
+# _This is italic text with a heading._
+
+## **This is bold text with a heading.**
+
+### This is regular text with a heading.
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+
+        self.assertEqual(
+            html,
+            "<div><h1><i>This is italic text with a heading.</i></h1><h2><b>This is bold text with a heading.</b></h2><h3>This is regular text with a heading.</h3></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_quote(self):
+        md = """
+
+>_This is italic text with a heading._
+>**This is bold text with a heading.**
+
+### This is regular text with a heading.
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+
+        self.assertEqual(
+            html,
+            "<div><blockquote><i>This is italic text with a heading.</i>\n<b>This is bold text with a heading.</b></blockquote><h3>This is regular text with a heading.</h3></div>",
+        )
+
+
+    def test_unordered(self):
+        md = """
+
+- _This is italic text with a heading._
+- **This is bold text with a heading.**
+
+### This is regular text with a heading.
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+
+        #print(f"\n\n{html}\n\n")
+
+        self.assertEqual(
+            html,
+            "<div><ul><li><i>This is italic text with a heading.</i></li>\n<li><b>This is bold text with a heading.</b></li></ul><h3>This is regular text with a heading.</h3></div>",
+        )
+
+    def test_ordered(self):
+        md = """
+
+1. _This is italic text with a heading._
+2. **This is bold text with a heading.**
+
+### This is regular text with a heading.
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+
+        #print(f"\n\n{html}\n\n")
+
+        self.assertEqual(
+            html,
+            "<div><ol><li><i>This is italic text with a heading.</i></li>\n<li><b>This is bold text with a heading.</b></li></ol><h3>This is regular text with a heading.</h3></div>",
+        )
 
 
 
